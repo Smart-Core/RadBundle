347ed02e8064b945dfccd5e0b67ab6aac4e47f19
artemdigi <artem.digi@gmail.com>
2022-09-06 14:12:01 +0700

    add DeletedAtTz

---
+++ Doctrine/ColumnTrait/DeletedAtTz.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartCore\RadBundle\Doctrine\ColumnTrait;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait DeletedAtTz
+{
+    /**
+     * @ORM\Column(type="datetimetz_immutable", nullable=true)
+     */
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: true)]
+    protected ?\DateTimeImmutable $deleted_at = null;
+
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deleted_at?->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+    }
+
+    public function setDeletedAt(?\DateTimeImmutable $deleted_at): self
+    {
+        $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+
+    public function setIsDeleted(bool $is_deleted): self
+    {
+        $this->deleted_at = $is_deleted ? new \DateTimeImmutable() : null;
+
+        return $this;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->deleted_at ? true : false;
+    }
+
+    public function getIsDeleted(): bool
+    {
+        return $this->isDeleted();
+    }
+
+    public function getIsDeletedAsText(): string
+    {
+        return $this->deleted_at ? 'Yes' : 'No';
+    }
+}
